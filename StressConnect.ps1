f97d9b381bef4ff345b9473a06efc55e85a6c9d4
Austin Wood <53714369+AustinEWood@users.noreply.github.com>
2023-08-19 00:38:01 -0500

    Adding Powershell  File for tool.

---
+++ StressConnect.ps1
@@ -0,0 +1,55 @@
+# Filtering for established connections removing loopbacks & IP6 addresses
+$netstatOutput = netstat -ano | findstr ESTABLISHED | findstr -v 127. | findstr -v [
+
+# Parse the netstat output to extract connection information
+$connections = $netstatOutput | ForEach-Object {
+    $parts = $_ -split '\s+'
+    $addressParts = $parts[2, 3] -split ':'
+    $remoteAddress = $addressParts[2]
+    $remotePort = $addressParts[3]
+
+    $addressParts = $parts[2] -split ':'
+    $localAddress = $addressParts[0]
+    $localPort = $addressParts[1]
+    
+    [PSCustomObject]@{
+        Protocol = $parts[1]
+        LocalAddress = $localAddress
+        LocalPort = $localPort
+        RemoteAddress = $remoteAddress
+        RemotePort = $remotePort
+        State = $parts[4]
+        PID = $parts[5]
+    }
+}
+
+# Initialize an array to store processed connection information
+$connectionsWithProcesses = @()
+
+# Loop through each connection to process VirusTotal data
+foreach ($connection in $connections) {
+    $process = Get-Process -Id $connection.PID -ErrorAction SilentlyContinue
+
+    # Construct the VirusTotal API URL for the current connection's remote address
+    $virusTotalApiUrl = "https://www.virustotal.com/api/v3/ip_addresses/$($connection.remoteAddress)"
+    
+    # Set up headers with your API key
+    $headers = @{
+        "x-apikey" = "8e52cac007ddd074c33334f14a4a0f2100668bc2418640ccf57685f080064abc"
+    }
+
+    # Send the VirusTotal API request
+    $virusTotalResponse = Invoke-RestMethod -Uri $virusTotalApiUrl -Headers $headers
+
+    $connection | Add-Member -MemberType NoteProperty -Name ProcessName -Value ($process.Name -join ', ')
+    $connection | Add-Member -MemberType NoteProperty -Name MaliciousVerdicts -Value ($virusTotalResponse.data.attributes.last_analysis_stats.malicious)
+    
+    # Add the processed connection information to the array
+    $connectionsWithProcesses += $connection
+}
+
+# Export the data to a CSV file
+$csvFilePath = "C:\Users\Austi\Documents\IPs\ConnectionTest.csv"
+$connectionsWithProcesses | Export-Csv -Path $csvFilePath -NoTypeInformation
+
+Write-Host "Data exported to $csvFilePath"
